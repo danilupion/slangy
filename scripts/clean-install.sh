04e33d98286239ca2964a55e93ad16c6584c6349
Dani Lupión <danilupion@gmail.com>
2023-09-30 11:54:02 +0200

    Dependencies bump

--- scripts/clean-install.sh
+++ scripts/clean-install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 rm -rf node_modules packages/*/node_modules apps/*/node_modules;
+
+# Check if --delete-lock or -dl parameter is provided, if so delete yarn.lock
+if [[ $1 == "--delete-lock" ]] || [[ $1 == "-dl" ]]; then
+  rm -f yarn.lock;
+fi
+
 yarn;
