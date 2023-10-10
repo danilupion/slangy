49cd8737d6565f86d24159035909063256ac58be
Dani Lupión <danilupion@gmail.com>
2023-10-10 18:30:13 +0200

    Helper type for serializable items

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -9,3 +9,17 @@ export type FieldsWithMaybePromise<T> = {
 export type ArrayElement<T> = T extends (infer E)[] ? E : never;
 
 export type Optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+
+type Serializable = string | number | boolean | Date | SerializableObject | SerializableArray;
+type SerializableObject = { [key: string]: Serializable };
+type SerializableArray = Array<Serializable>;
+
+export type Serialized<T> = {
+  [K in keyof T]: T[K] extends Array<infer U>
+    ? Array<Serialized<U>>
+    : T[K] extends Date
+    ? string
+    : T[K] extends object
+    ? Serialized<T[K]>
+    : T[K];
+};
